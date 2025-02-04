43f9da35f092dc91666c9516a4a8ae0980fd47bb
20:31 <glebshapov2@mail.ru>
2025-02-04 18:18:10 +0300

    Added fields for Card. Closed #12 (#18)

--- finance-manager/card.h
+++ finance-manager/card.h
@@ -1,7 +1,16 @@
 #pragma once
 
 
+#include <iostream>
+#include <string>
+
 class Card
 {
-
+private:
+	int _balance;
+	int _pin;
+	tm _data{};
+	int _cvv;
+	std::string _number;
+	std::string _userName;
 };
