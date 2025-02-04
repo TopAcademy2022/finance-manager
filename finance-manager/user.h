e075025cebae207163bf89ac50164a5f6dcab284
synjoke <azilka458@gmail.com>
2025-02-04 18:02:20 +0300

    Add fields for User. Closed #13

--- finance-manager/user.h
+++ finance-manager/user.h
@@ -1,6 +1,16 @@
 #pragma once
-
+#include <string>
+#include "wallet.h"
+using namespace std;
 
 class User
 {
+public:
+
+private:
+	Wallet _UserWallet;
+	string _login;
+	string _password;
+	string _FIO;
+	tm _UserDate{};  
 };
