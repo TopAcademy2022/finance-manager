c653dfab48468883672a720aa0a69350e48e2ff9
lxaesq <n08671876@gmail.com>
2025-02-06 17:48:20 +0300

    Added fields for Cost. Closed #9

--- finance-manager/cost.h
+++ finance-manager/cost.h
@@ -5,4 +5,6 @@
 
 class Cost : public CardOperation
 {
+private:
+	enum class _categories{Beauty, Fast_Food, Clothes_And_Shoes, Transactions, Supermarkets, Pharmacies, Entertainments, Fuels, Sporting_Goods};
 };
