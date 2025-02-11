0a807e3c7cb2be0c437d65da7a0be4f673067f6a
synjoke <azilka458@gmail.com>
2025-02-11 18:08:55 +0300

    Added Registration. Closed #28

--- finance-manager/finance-manager.cpp
+++ finance-manager/finance-manager.cpp
@@ -1,7 +1,9 @@
 ﻿#include "menu.h"
-
+#include "user.h"
 int main()
 {
     Menu menu = Menu();
     menu.PrintLogin();
+
+
 }
--- finance-manager/user.cpp
+++ finance-manager/user.cpp
@@ -1,6 +1,37 @@
 #include "user.h"
 
-bool User::Login(std::string login, std::string password)
+//std::string User::tm_to_date(std::string format)
+//{
+//	std::string thing;
+//	if(format == "dd-mm-yyyy")
+//	{
+//		thing = std::to_string(this->_birthDate.tm_wday)  + "."+std::to_string(this->_birthDate.tm_mon)+"." + std::to_string(this->_birthDate.tm_year);
+//	}
+//	return thing;
+//}
+
+bool User::Registration(std::string login, std::string password, std::string fio, tm birthdate)
 {
-    return false;
+	std::string defaultFormat = "dd-mm-yyyy";
+	std::ofstream file;
+	file.open("./Users.txt", std::ios::app);
+	if (file.is_open()) {
+		try {
+			std::string text = login + " " + password + " " + fio + " " + std::to_string(birthdate.tm_wday) + "." + std::to_string(birthdate.tm_mon) + "." + std::to_string(birthdate.tm_year) + "\n";
+
+			file.write(text.c_str(), text.size());
+			file.close();
+			std::cout << "Succes.";
+			return true;
+		}
+		catch (std::exception ex) {
+			return false;
+		}
+	}
+	else {
+		std::cout << "File cant be open." << std::endl;
+		return false;
+	}
+	
+
 }
