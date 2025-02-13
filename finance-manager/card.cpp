3dd13fadebf69c6d4f2b80b665644404ba1f2a0f
bbbgviff <ksyu.sl08@yandex.ru>
2025-02-13 16:28:59 +0300

    Added TopUp. Closed #25

--- finance-manager/card.cpp
+++ finance-manager/card.cpp
@@ -1,1 +1,14 @@
 #include "card.h"
+
+bool Card::TopUp(int sum)
+{
+	if (sum > 0) {
+		this->_balance += sum;
+		return 1;
+		std::cout << "The balance has been successfully replenished." << std::endl;
+	}
+	else {
+		std::cerr << "It is not possible to top up the balance for this amount. :: cerr\n";
+		return 0;
+	}
+}
