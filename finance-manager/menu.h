91da3a6fd25aeb6a5571174ccca7e3efa5e88a3a
sonya678 <sofachkarusanova@gmail.com>
2025-02-24 17:52:23 +0300

    Added documentation for Menu. Closed #53 (#61)

--- finance-manager/menu.h
+++ finance-manager/menu.h
@@ -5,13 +5,26 @@
 #include <type_traits>
 #include "user.h"
 
+/*!
+* @class Menu representing the application menu
+* @brief Application menu
+*/
 class Menu
 {
 private:
+	/*!
+	* @brief User of our system.
+	*/
 	User _user;
 
+	/*!
+	* @brief Displays the main menu of the application.
+	* @detail Displays a list of available menu options on the screen.
+	*/
 	void Print();
 public:
+	/*!
+	* @brief Allows you to log in or register.
+	*/
 	void PrintLogin();
-
 };
