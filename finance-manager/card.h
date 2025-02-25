fd149c7f9d0c08ab77dc10132a4b70c29172b5e7
20:31 <glebshapov2@mail.ru>
2025-02-25 17:04:42 +0300

    Added documentation for Card. Closed #40 (#62)

--- finance-manager/card.h
+++ finance-manager/card.h
@@ -4,17 +4,22 @@
 #include <iostream>
 #include <string>
 
+//@brief class representing a bank card
 class Card
 {
 private:
-	int _balance;
-	int _pin;
-	tm _data{};
-	int _cvv;
-	std::string _number;
-	std::string _userName;
+	int _balance; ///< current card balance	
+	int _pin; ///< card PIN
+	tm _data{}; ///< up to
+	int _cvv; ///< card CVV code
+	std::string _number; ///< card number 
+	std::string _userName; ///< cardholder's name
 
 public:
+	/*!
+	* @brief The method allows you to replenish the card balance by the specified amount.
+	* @return True If the refill was successful. False If the replenishment amount is incorrect (e.g. negative).
+	*/
 	bool TopUp(int sum);
   
 	std::string GetUserName() {
