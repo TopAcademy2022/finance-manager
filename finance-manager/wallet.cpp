fde0c87a238a489c2ccbc25852c1e258256ca285
20:31 <glebshapov2@mail.ru>
2025-03-04 17:24:27 +0300

    Added GetCard. Closed #66 (#71)

--- finance-manager/wallet.cpp
+++ finance-manager/wallet.cpp
@@ -5,6 +5,11 @@ void Wallet::AddCard(Card card)
 	_cardList.push_back(card);
 }
 
+std::list<Card> Wallet::GetCardList()
+{
+    return this->_cardList;
+}
+
 void Wallet::PrintCards() {
     for (Card r : _cardList) {
         std::cout << r.GetUserName() << r.GetBalance() << std::endl; 
